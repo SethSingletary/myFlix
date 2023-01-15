dff6be8d20316d2d95a69bc2fbacad8b3688bd7f
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-01-14 22:05:52 -0500

    added morgan to app

---
+++ index.js
@@ -0,0 +1,13 @@
+const { response } = require('express');
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Welcome to my book club!\n');
+}).listen(8080);
+
+console.log('My first node server is running on Port 8080.');
