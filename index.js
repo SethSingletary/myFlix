77700a5d74b9a143d04472cfe69e4b9ad1e8f99b
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-01-15 17:44:04 -0500

    added app.get

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 
+let topMovies = [
+    {
+        title: 'Star wars',
+        author: 'LucasFilms'
+    }
+];
+
 const http = require('http');
 
 http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Welcome to my book club!\n');
 }).listen(8080);
+
+app.get('/movies', (req, res) =>{
+    res.json(topMovies);
+    console.log('movies!!');
+})
+app.get('/', (req, res) =>{
+    console.log('default response');
+})
 
 console.log('My first node server is running on Port 8080.');
