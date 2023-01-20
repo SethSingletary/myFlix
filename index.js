39cb64481111a652d7ba4da49a38045ee663c750
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-01-19 21:23:07 -0500

    error with data for whatever reason

--- index.js
+++ index.js
@@ -18,6 +18,34 @@ let topMovies = [
     }
 ];
 
+app.get('/movies', (req, res) => {
+    res.send('Successful GET request returning data on all movies');
+  });
+app.get('/movies/:title', (req, res) => {
+    res.send('Successful GET request returning data on chosen movie');
+  });
+app.get('/movies/genres/:title', (req, res) => {
+    res.send('Successful GET request returning data on chosen movies genre');
+  });
+app.get('/movies/directors', (req, res) => {
+    res.send('Successful GET request returning data on chosen movies director');
+  });
+app.post('/users', (req, res) => {
+    res.send('Successful POST request creating new user');
+  });
+app.get('/users/:id', (req, res) => {
+    res.send('Successful PUT request updating user data');
+  });
+app.get('/users/:id/:movieTitle', (req, res) => {
+    res.send('Successful DELETE request deleting movie from favorites');
+  });
+app.get('/users/:id', (req, res) => {
+    res.send('Successful DELETE request deleting user');
+  });
+
+
+
+
 app.get('/movies', (req, res) =>{
     res.json(topMovies);
     console.log('movies!!');
